b8ff704da289dd5046289609184679aade8a72c7
yihe.hua <yihe.hua@torc.ai>
2023-10-18 23:26:32 +0000

    update scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 DIR=fsdv2
 WORK=work_dirs
-CONFIG=fsdv2_torc_2x
+# CONFIG=fsdv2_torc_2x
+CONFIG=fsdv2_nusc_2x
 bash tools/dist_train.sh configs/$DIR/$CONFIG.py 4 --work-dir ./$WORK/$CONFIG/ --cfg-options evaluation.jsonfile_prefix=./$WORK/$CONFIG/results evaluation.metric=bbox --launcher pytorch --seed 1
